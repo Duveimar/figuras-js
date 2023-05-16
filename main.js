682830d4e828355088704b7ce0456fc212e9501f
Duveimar <72568140+Duveimar@users.noreply.github.com>
2023-05-16 12:42:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function circulo(){
+    let figura=document.getElementById("figura");
+    /*figura.classList.add("circulo"); /*agrega clases al elemento*/
+    figura.classList.toggle("circulo") /*agrega y quita la clase con cada click*/
+}
+
+function rectangulo(){
+    let figura=document.getElementById("figura");
+    /*figura.classList.add("circulo"); /*agrega clases al elemento*/
+    figura.classList.toggle("rectangulo") /*agrega y quita la clase con cada click*/
+}
